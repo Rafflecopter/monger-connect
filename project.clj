c0a7d0e1ed58603ef3daa0c412471c8ad28456cf
Jon Eisen <jon@metricfeat.com>
2015-08-05 14:34:05 -0600

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject monger-connect "0.1.0"
+(defproject monger-connect "0.1.1-SNAPSHOT"
   :description "Making it easier to connect with monger"
   :url "http://github.com/Rafflecopter/monger-connect"
   :license {:name "MIT"
